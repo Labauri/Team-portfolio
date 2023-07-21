ee0bec80f0c016733ada478fb4e1106ae28eede7
Labauri <111067293+atmmmmmm@users.noreply.github.com>
2023-07-21 22:42:31 +0300

    Added new page 'Another' and added some picture.

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 * `Programmer`: [Artem Labauri](https://github.com/Labauri).
 * `Web-Designer`: [Danil Demyanov](https://github.com/Exclusiveexxe).
-* `Web-Designer`: [Daniil Tsvigun](https://vk.com/d_tsvigun).
+* `Web-Designer`: [Daniil Tsvigun](https://github.com/User0592).
 
 ---
--- port.js
+++ port.js
@@ -1,13 +1,47 @@
-// Найти элементы с классами "section-ellipse__arrow" и "section-arrow__see"
-var ellipseArrow = document.querySelector('.section-ellipse__arrow');
-var arrowSee = document.querySelector('.section-arrow__see');
+// Получаем ползунок и контейнер с пагинацией
+var slider = $('.pagination-slider');
+var paginationContainer = $('.pagination-container');
+var pages = paginationContainer.find('.pagination-item');
 
-// Создать псевдоэлемент ::before для элемента "section-ellipse__arrow"
-var ellipseArrowBefore = document.createElement('div');
-ellipseArrowBefore.classList.add('section-ellipse__arrow-before');
-ellipseArrow.appendChild(ellipseArrowBefore);
+// Функция для обработки переключения страницы
+function changePage(destination) {
+  // Получаем порядковый номер страницы, чтобы определить положение ползунка
+  var targetIndex = getTargetPageIndex(destination);
 
-// Создать псевдоэлемент ::after для элемента "section-arrow__see"
-var arrowSeeAfter = document.createElement('div');
-arrowSeeAfter.classList.add('section-arrow__see-after');
-arrowSee.appendChild(arrowSeeAfter);
+  // Получаем ширину контейнера с пагинацией
+  var containerWidth = paginationContainer.width();
+
+  // Вычисляем значение сдвига для ползунка
+  var sliderOffset = (targetIndex / 2) * (containerWidth / 100);
+
+  // Анимация эффекта перехода
+  var pageTransition = $('.page-transition');
+  pageTransition.css('transform', 'translateX(0)');
+  setTimeout(function() {
+    pageTransition.css('transform', 'translateX(-100%)');
+  }, 100); // Небольшая задержка перед запуском анимации
+
+  // Плавно перемещаем ползунок с помощью анимации
+  slider.animate({
+    'transform': 'translateX(' + sliderOffset + 'px)',
+  }, 100); // 100 миллисекунд - время анимации
+
+  // Задержка перед переходом на страницу
+  setTimeout(function () {
+    window.location.href = destination;
+  }, 100); // Задержка больше, чем анимация эффекта перехода (0.1 секунда)
+}
+
+// Функция для получения порядкового номера страницы по её ссылке
+function getTargetPageIndex(pageLink) {
+  var pageIndex = 0;
+  // Ищем нужную страницу и получаем её порядковый номер
+  pages.each(function (index) {
+    if ($(this).attr('onclick').includes(pageLink)) {
+      pageIndex = index;
+      return false; // Прерываем цикл, если страница найдена
+    }
+  });
+  return pageIndex;
+}
+
